a84762128959881f4011b437b7cf43942ad656b6
Bishwas Chaudhary <bishwas.cdyy@gmail.com>
2025-09-17 16:53:44 +0545

    tsk

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -20,21 +20,11 @@ const Projects = () => {
 				<div className="content-wrapper">
 					<div className="projects-container">
 						<div className="title projects-title">
-							Things I’ve made trying to put my dent in the
-							universe.
+							Projects I've thrown into the void
 						</div>
 
 						<div className="subtitle projects-subtitle">
-							I've worked on a variety of projects over the years
-							and I'm proud of the progress I've made. Many of
-							these projects are open-source and available for
-							others to explore and contribute to. If you're
-							interested in any of the projects I've worked on,
-							please feel free to check out the code and suggest
-							any improvements or enhancements you might have in
-							mind. Collaborating with others is a great way to
-							learn and grow, and I'm always open to new ideas and
-							feedback.
+							Basically, I spend an unhealthy amount of time writing code that only sometimes works and then call it a “project.” A few of these disasters are open-source, which means you can not only laugh at my mistakes but also fix them if you're feeling brave. If you've got better ideas (spoiler: you do), feel free to jump in. I'll pretend to be open to feedback while secretly crying over merge conflicts.
 						</div>
 
 						<div className="projects-list">
